805e6450e586f35e8740fdea0c0ac9e237b248cb
killshotxd <hassanansari211@gmail.com>
2022-09-18 14:54:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const app = require("./app");
+// const app = require("./app");
 
-//31:45
-console.log(app.x);
-console.log(app.y);
+// 31:45
+// console.log(app.x);
+// console.log(app.y);
+//40:00
+
+const arr = [2, 3, 45, 6, 6, 7, 7, 2, 3, 8];
+// let result = arr.filter((item) => {
+//   return item === 3;
+// });
+
+// let result = arr.filter((item) => {
+//   return item >= 3;
+// });
+
+// const setItem = new Set(result);
+
+console.log(result);
+// console.log(setItem);
