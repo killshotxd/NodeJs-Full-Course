30ea68f09b077c0d931c5bd4b56a8e0ae2153042
killshotxd <hassanansari211@gmail.com>
2022-09-18 15:11:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,9 +44,19 @@
 // Making basic server
 
 const http = require("http");
-http
-  .createServer((req, res) => {
-    res.write("<h1>Hello this is Killshot</h1>");
-    res.end();
-  })
-  .listen(4500);
+
+// http
+//   .createServer((req, res) => {
+//     res.write("<h1>Hello this is Killshot</h1>");
+//     res.end();
+//   })
+//   .listen(4500);
+
+// BTS--------
+
+// const dataControl = (req, res) => {
+//   res.write("<h1>Hello this is Killshot</h1>");
+//   res.end();
+// };
+
+// http.createServer(dataControl).listen(4500);
