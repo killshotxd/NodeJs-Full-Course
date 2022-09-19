52cb9c7b16bb9d22174d2aaabb3b18e7e6516679
killshotxd <hassanansari211@gmail.com>
2022-09-19 16:33:41 +0530

    argv 2

--- index.js
+++ index.js
@@ -126,3 +126,16 @@
 // const input = process.argv;
 
 // fs.writeFileSync(input[2], input[3]);
+
+const fs = require("fs");
+
+const input = process.argv;
+
+if (input[2] == "add") {
+  fs.writeFileSync(input[3], input[4]);
+} else if (input[2] == "remove") {
+  // Removing file from system
+  fs.unlinkSync(input[3]);
+} else {
+  console.log("Invalid");
+}
