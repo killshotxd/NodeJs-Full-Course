1f26c931cdbe0fa07d6b092f847a34ce3f6856dc
killshotxd <hassanansari211@gmail.com>
2022-09-23 17:13:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -275,15 +275,15 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello , This is Home page");
+  res.send("Welcome , This is Home page");
 });
 
 app.get("/about", (req, res) => {
-  res.send("Hello , This is About page");
+  res.send("Welcome , This is About page");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Hello , This is Help page");
+  res.send("Welcome , This is Help page");
 });
 
 app.listen(5000);
